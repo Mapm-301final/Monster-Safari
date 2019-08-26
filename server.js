3e0e03714406ca16d2e564287de32274dbf42c56
mts513 <34582160+mts513@users.noreply.github.com>
2019-08-26 16:25:34 -0700

    installed method override

--- server.js
+++ server.js
@@ -8,3 +8,25 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+//superagent
+const superagent = require('superagent');
+require('dotenv').config();
+
+//=================Postgres Database===============
+// const pg = require('pg');
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.connect();
+// client.on('error', err=> console.error(err));
+
+//setting up ejs
+app.set('view engine', 'ejs');
+app.use(express.static('./public/../'));
+
+app.use(express.urlencoded({extended:true}));
+
+//PORT
+const PORT = process.env.PORT || 3000;
+
+//tells our server to start listening on the port
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
