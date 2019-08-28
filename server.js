ec69bdc36d530a532f85fe5b2d4bea6d2dd7363d
ahrenswett <ahrenswett@gmail.com>
2019-08-27 21:48:11 -0700

    working on getLocation/geocode and added pic of whiteboard drawing i did

--- server.js
+++ server.js
@@ -54,18 +54,18 @@ app.get('/', (request, response)=>{
 //===============================================================================================//
 //**************************************     Functions     ************************************//
 // locatiton= Geolocation.getCurrentPosition()
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    console.log('Geolocation is not supported by this browser.');
-  }
-}
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     console.log('Geolocation is not supported by this browser.');
+//   }
+// }
 
-let showPosition=(position)=>{
-  console.log('Latitude: '+ position.coords.latitude + 
-  '  Longitude: ' + position.coords.longitude);
-};
+// let showPosition=(position)=>{
+//   console.log('Latitude: '+ position.coords.latitude + 
+//   '  Longitude: ' + position.coords.longitude);
+// };
 
 
 // getWeather(request, response)
