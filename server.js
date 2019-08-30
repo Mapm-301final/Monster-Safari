3678411b34443a04282708d53e872e04dec9493c
ahrenswett <ahrenswett@gmail.com>
2019-08-29 19:34:44 -0700

    added new js files for location

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+// DATABASE_URL=postgres://localhost:5432/monster
+
 //Express
 const express = require('express');
 const app = express();
@@ -16,10 +19,12 @@ require('dotenv').config();
 
 
 //=================Postgres Database===============
-// const pg = require('pg');
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
-// client.on('error', err=> console.error(err));
+// DATABASE_URL=postgres://localhost:5432/monster
+
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err=> console.error(err));
 
 
 //setting up ejs
@@ -46,6 +51,9 @@ app.get('/', (request, response)=>{
   response.render('pages/index');
 });
 
+//calls Geocode
+app.get('/location',searchLatLong);
+//get s weather based on results returned by google
 // app.get('/weather', (request, response));
 
 
@@ -54,6 +62,22 @@ app.get('/', (request, response)=>{
 //**************************************     Functions     ************************************//
 
 
+
+
+//Gets location based on search query
+function searchLatLong(query){
+  console.log(query,'somthing abnoxious');
+  const url =`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  //using our superagent library to get the proper data format
+  return superagent.get(url)
+
+  //then when we got the data from superagent create a new City object with the query (location name) and data (res.body.results[0]);
+    .then(res => {
+      // console.log(res);
+      // return res.body.results[0].data.geometry.location.lat,res.body.results[0].data.geometry.location.long ;
+    });
+
+}
 
 // getWeather(request, response)
 // request.send(res=>       )
