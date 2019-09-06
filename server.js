7d3a8a54f2815a461c4fa1c4401fb07074e37c9c
mts513 <34582160+mts513@users.noreply.github.com>
2019-09-05 21:05:45 -0700

    fixed actual location in server

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ function curLoc(request,response){
       console.log(res, 'hello body');
       let map =`https://maps.googleapis.com/maps/api/staticmap?center=${loc.clat}%2c%20${loc.clng}&zoom=13&size=600x300&markers=icon:${res.icon}%7Csize:large%7Ccolor:red%7C${loc.clat}%2c%20${loc.clng}&maptype=roadmap&key=${process.env.GEOCODE_API_KEY}`;//would like to have a map the pans into the location would have to be a series of maps with at timeout and an incrementer for the zoom
       console.log(map);
-      response.render('pages/found',{ map: map});
+      response.render('pages/found',{map: map, llama: res});
     });
 }
 // POKEMON API CALLS
